3065958af626ae5a8c99f4bdbb36dbe4182453ac
Jay Chiarella <jay.gch93@gmail.com>
2017-09-10 08:35:54 -0400

    last clean up before adding webpack

--- script.js
+++ script.js
@@ -29,12 +29,40 @@ function closeTab(tabIndex, tab_component) {
     chrome.tabs.remove(tabIndex);
 }
 
-// 
+// renders CONTAINER UI of tab
+function renderContainer() {
+    const tab_component = document.createElement('div');
+    tab_component.classList.add('tab');
+    return tab_component;
+}
+
+// renders URL UI of tab
+function renderTabComponentUrl(tabUrl) {
+    const tab_component_url = document.createElement('p');
+    tab_component_url.textContent = max40Characters(tabUrl);
+    return tab_component_url;
+}
+
+// renders TITLE UI of tab
+function renderTabComponentTitle(tabTitle) {
+    const tab_component_title = document.createElement('p');
+    tab_component_title.textContent = max40Characters(tabTitle);
+    return tab_component_title;
+}
+
+// renders (delete) BUTTON of tab
+function addDiscardButton() {
+    const discard_btn = document.createElement('button');
+    discard_btn.textContent = 'Close this tab';
+    discard_btn.classList.add('buttons');
+    return discard_btn;
+}
+
+//
 //
 function renderTabComponent(tabUrl, tabTitle, tabIndex) {
-
     // create container tab component
-    const tab_component = renderContainer()
+    const tab_component = renderContainer();
 
     // create url paragraph component
     const tab_component_url = renderTabComponentUrl(tabUrl);
@@ -58,35 +86,6 @@ function renderTabComponent(tabUrl, tabTitle, tabIndex) {
 
     // get tag destination and append the tab component
     document.getElementById('tabs').appendChild(tab_component);
-}
-
-// renders CONTAINER UI of tab
-function renderContainer(){
-    const tab_component = document.createElement('div');
-    tab_component.classList.add('tab');
-    return tab_component
-}
-
-// renders URL UI of tab
-function renderTabComponentUrl(tabUrl){
-    const tab_component_url = document.createElement('p');
-    tab_component_url.textContent = max40Characters(tabUrl);
-    return tab_component_url
-}
-
-// renders TITLE UI of tab 
-function renderTabComponentTitle(tabTitle){
-    const tab_component_title = document.createElement('p');
-    tab_component_title.textContent = max40Characters(tabTitle);
-    return tab_component_title
-}
-
-// renders (delete) BUTTON of tab
-function addDiscardButton(){
-    const discard_btn = document.createElement('button');
-    discard_btn.textContent = 'Close this tab';
-    discard_btn.classList.add('buttons');
-    return discard_btn
 }
 
 document.addEventListener('DOMContentLoaded', () => {
